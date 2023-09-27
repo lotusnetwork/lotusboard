b096802a22026767faef77625e10b7ca20b35f08
lotusnetwork <143520796+lotusnetwork@users.noreply.github.com>
2023-09-27 06:55:15 +0000

    init.sh updated

--- init.sh
+++ init.sh
@@ -3,8 +3,8 @@
 rm -rf composer.phar
 wget https://github.com/composer/composer/releases/latest/download/composer.phar -O composer.phar
 php composer.phar install -vvv
-while true
 counter=1
+while true
 do
   if curl mysql:3306 2>&1 | grep -qs "Received HTTP/0.9 when not allowed"; then
     echo "Continue to lotusboard setup"
