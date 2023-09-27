ba417475f27d9d92089bdbbf7e50ba163f329873
lotusnetwork <143520796+lotusnetwork@users.noreply.github.com>
2023-09-27 06:47:11 +0000

    init.sh updated

--- init.sh
+++ init.sh
@@ -3,6 +3,17 @@
 rm -rf composer.phar
 wget https://github.com/composer/composer/releases/latest/download/composer.phar -O composer.phar
 php composer.phar install -vvv
+while true
+counter=1
+do
+  if curl mysql:3306 2>&1 | grep -qs "Received HTTP/0.9 when not allowed"; then
+    echo "Continue to lotusboard setup"
+    break
+  fi
+  echo "Waiting for the database(database not ready yet) retry: $counter"
+  let counter++
+  sleep 1
+done
 php artisan v2board:install
 
 #if [ -f "/etc/init.d/bt" ]; then
